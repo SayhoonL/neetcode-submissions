aa5b1df63077cdfb71544570321772030667d846
SayhoonL <97651310+SayhoonL@users.noreply.github.com>
2026-08-20 14:05:22 -0400

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        function isValid(char){
+            if(char >= "A" && char <= "Z"){
+                return true
+            }
+            if(char >= "a" && char <= "z"){
+                return true
+            }
+            if(char >= "0" && char <= "9"){
+                return true
+            }
+            return false
+        }
+        let left = 0
+        let right = s.length - 1
+        while(left < right){
+            
+            while(left < right && !isValid(s[left])){
+                left++
+            }
+            while(left < right && !isValid(s[right])){
+                right--
+            }
+            console.log(s[left], s[right])
+            if(s[left].toLowerCase() !== s[right].toLowerCase()){
+                return false
+            }
+            left++
+            right--
+        }
+        return true
+    }
+}
