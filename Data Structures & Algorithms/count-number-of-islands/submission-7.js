bebc5351a44102f17ff695bbd4de6b483fa997f9
Sayhoon Lee <97651310+SayhoonL@users.noreply.github.com>
2026-09-09 19:59:55 -0400

    Add: count-number-of-islands - submission-7

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-7.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {character[][]} grid
+     * @return {number}
+     */
+    numIslands(grid) {
+        let count = 0
+        for(let i = 0; i < grid.length; i++){
+            for(let j = 0; j < grid[0].length; j++){
+                if(grid[i][j] === "1"){
+                    dfs(i,j)
+                    count++
+                }
+            }
+        }
+
+        function dfs(r,c){
+            if(r < 0 || r >= grid.length) return
+            if(c < 0 || c >= grid[0].length) return
+            if(grid[r][c] === "0") return
+            grid[r][c] = "0"
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+        }
+
+        return count
+    }
+}
