774eab4258aa3ee4dae7f27545eefbc5c5b00f7a
Sayhoon Lee <97651310+SayhoonL@users.noreply.github.com>
2026-09-11 19:34:19 -0400

    Add: cheapest-flight-path - submission-1

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} flights
+     * @param {number} src
+     * @param {number} dst
+     * @param {number} k
+     * @return {number}
+     */
+    findCheapestPrice(n, flights, src, dst, k) {
+        let prices = new Array(n).fill(Infinity)
+        prices[src] = 0
+        for(let i = 0; i <= k; i++){
+            let temp = [...prices]
+            for(let [from,to,cost] of flights){
+                if(prices[from] === Infinity) continue
+                temp[to] = Math.min(temp[to],prices[from] + cost)
+            }
+
+            prices = temp
+        }
+
+        return prices[dst] === Infinity ? -1 : prices[dst]
+    }
+}
