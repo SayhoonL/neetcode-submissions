497a3d1486b575c82c15bd9a265ef345a14a47fa
Sayhoon Lee <97651310+SayhoonL@users.noreply.github.com>
2026-09-11 18:43:31 -0400

    Add: products-of-array-discluding-self - submission-5

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-5.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let arr = new Array(nums.length).fill(1)
+        let prefix = 1
+        for(let i = 0; i < nums.length; i++){
+            arr[i] = prefix
+            prefix *= nums[i]
+        }
+        prefix = 1
+        for(let i = nums.length - 1; i >= 0; i--){
+            arr[i] *= prefix 
+            prefix *= nums[i]
+        }
+
+        return arr
+
+    }
+}
