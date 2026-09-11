54625250196d956b3c2f901ddcfae317166d1d4c
Sayhoon Lee <97651310+SayhoonL@users.noreply.github.com>
2026-09-11 18:34:35 -0400

    Add: string-encode-and-decode - submission-8

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-8.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let result = ""
+        for(let str of strs){
+            result+= str.length
+            result+= "#"
+            result+= str
+        }
+        console.log(result)
+        return result
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let result = []
+        let i = 0
+        while(i < str.length){
+            let j = i
+            while(str[j] !== "#"){
+                j++
+            }
+            let length = Number(str.slice(i,j))
+            result.push(str.slice(j + 1, j + 1 + length))
+            i = j + 1 + length
+        }
+
+        return result
+    }
+}
