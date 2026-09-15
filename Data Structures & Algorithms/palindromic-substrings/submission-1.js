5c681a0fe64fdca83f587c4f3bd08dc736c8a7b0
Sayhoon Lee <97651310+SayhoonL@users.noreply.github.com>
2026-09-15 00:12:47 -0400

    Add: palindromic-substrings - submission-1

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-1.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    countSubstrings(s) {
+        let count = 0
+
+        for(let i = 0; i < s.length; i++){
+            let l = i
+            let r = i
+            while(l <= r && l >= 0 && r < s.length && s[l] === s[r]){
+                count++
+                l--
+                r++
+            }
+        }
+        for(let i = 0; i < s.length; i++){
+            let l = i
+            let r = i + 1
+            while(l < r && l >= 0 && r < s.length && s[l] === s[r]){
+                count++
+                l--
+                r++
+            }
+        }
+
+        return count
+    }
+}
