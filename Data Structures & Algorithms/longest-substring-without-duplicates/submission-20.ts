a17b3904a566d310d8584d3d9602c5e72bafc2c6
Sayhoon Lee <97651310+SayhoonL@users.noreply.github.com>
2026-09-15 19:02:56 -0400

    Add: longest-substring-without-duplicates - submission-20

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-20.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s: string): number {
+        if(s.length === 0) return 0
+        let set = new Set()
+        let l = 0
+        let r = 1
+        let result = 1
+        set.add(s[0])
+        while(r < s.length){
+            if(set.has(s[r])){
+                while(set.has(s[r])){
+                    set.delete(s[l])
+                    l++
+                }
+            }
+            result = Math.max(r - l + 1, result)
+            set.add(s[r])
+            r++
+        }
+
+        return result
+    }
+}
