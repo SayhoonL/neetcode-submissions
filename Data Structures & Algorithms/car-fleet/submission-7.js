90726548e1c845e96705152b405579cb9d705f74
Sayhoon Lee <97651310+SayhoonL@users.noreply.github.com>
2026-09-16 05:01:53 -0400

    Add: car-fleet - submission-7

---
+++ Data Structures & Algorithms/car-fleet/submission-7.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        let arr = []
+        for(let i = 0; i < position.length; i++){
+            let pos = position[i]
+            let spe = speed[i]
+            arr.push([pos,spe])
+        }
+        arr.sort((a,b) => a[0] - b[0])
+        let hour = 0
+        let count = 0
+        while(arr.length > 0){
+            let [pos,spe] = arr.pop()
+            if(hour < (target - pos)/spe){
+                hour = (target - pos)/spe
+                count++
+            }
+
+        }
+
+        return count
+    }
+}
