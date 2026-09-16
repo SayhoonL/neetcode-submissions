cd8f425324d0b9f406db2915c85b85805186a009
Sayhoon Lee <97651310+SayhoonL@users.noreply.github.com>
2026-09-16 12:40:10 -0400

    Add: buildings-with-an-ocean-view - submission-0

---
+++ Data Structures & Algorithms/buildings-with-an-ocean-view/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number[]}
+     */
+    findBuildings(heights) {
+        let result = []
+        let max = heights[heights.length - 1]
+        result.push(heights.length - 1)
+        for(let i = heights.length - 1; i >= 0; i--){
+            if(heights[i] > max){
+                result.push(i)
+            }
+            max = Math.max(max, heights[i])
+        }
+
+        return result.reverse()
+    }
+}
