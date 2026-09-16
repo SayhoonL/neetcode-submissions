046833a5cdb11345dcb870fa37ea1bde095cc98f
Sayhoon Lee <97651310+SayhoonL@users.noreply.github.com>
2026-09-16 11:21:08 -0400

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} digits
+     * @return {number[]}
+     */
+    plusOne(digits) {
+        let number = ""
+        for(let digit of digits){
+            number = number + String(digit)
+        }
+
+        number = Number(number) + 1
+        number = String(number)
+        let result = []
+        for(let i = 0; i < number.length; i++){
+            result.push(number[i])
+        }
+
+        return result
+    }
+}
